fdff58f13c863c03cc9270ad435f045708c86b4a
Spentine <spentines@gmail.com>
2025-09-01 03:27:25 -0700

    make time update display

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
 function main() {
+  const buttonContainer = document.getElementById("buttonContainer");
   const mainButton = document.getElementById("mainButton");
-  const mainText = document.getElementById("mainText");
-  const clickEffect = document.getElementById("clickEffect");
+  const mainContent = document.getElementById("mainContent");
+  const clickEffectContainer = document.getElementById("clickEffectContainer");
+  
+  function addMainContent() {
+    const elementType = "div";
+    const hourElement = document.createElement(elementType);
+    const minuteElement = document.createElement(elementType);
+    const secondElement = document.createElement(elementType);
+    
+    hourElement.classList.add("number");
+    minuteElement.classList.add("number");
+    secondElement.classList.add("number");
+    
+    const colon1 = document.createElement(elementType);
+    const colon2 = document.createElement(elementType);
+
+    colon1.classList.add("colon");
+    colon2.classList.add("colon");
+
+    colon1.textContent = ":";
+    colon2.textContent = ":";
+
+    mainContent.appendChild(hourElement);
+    mainContent.appendChild(colon1);
+    mainContent.appendChild(minuteElement);
+    mainContent.appendChild(colon2);
+    mainContent.appendChild(secondElement);
+    
+    return [hourElement, minuteElement, secondElement];
+  }
+  
+  const [hour, minute, second] = addMainContent();
+  
+  function updateMainContent() {
+    const currentHour = new Date().getHours();
+    const currentMinute = new Date().getMinutes();
+    const currentSecond = new Date().getSeconds();
+    
+    hour.textContent = String(currentHour).padStart(2, "0");
+    minute.textContent = String(currentMinute).padStart(2, "0");
+    second.textContent = String(currentSecond).padStart(2, "0");
+  }
+  
+  updateMainContent();
   
   let unit = "dvw";
-  
   function render() {
     // keep the button in bounds
-    const bounds = mainButton.getBoundingClientRect();
-    // console.log(bounds);
+    const bounds = buttonContainer.getBoundingClientRect();
     
     if (bounds.top < 0) {
       unit = "dvh";
     } else if (bounds.top > 0 || bounds.width !== bounds.height) {
       unit = "dvw";
     }
-    mainButton.style.width = `100${unit}`;
-    mainButton.style.height = `100${unit}`;
-    mainButton.style.borderRadius = `50${unit}`;
-    mainText.style.fontSize = `16${unit}`;
+    
+    buttonContainer.style.width = `100${unit}`;
+    buttonContainer.style.height = `100${unit}`;
+    mainButton.style.width = `80${unit}`;
+    mainButton.style.height = `80${unit}`;
+    mainButton.style.borderRadius = `40${unit}`;
+    mainContent.style.fontSize = `16${unit}`;
+    
+    updateMainContent();
 
     window.requestAnimationFrame(render);
   }
